75a33156f97b9e0a6336dd444f6f07a2943025ec
Blair Fix <blairfix@gmail.com>
2020-05-13 07:33:41 -0400

    changed var names in top_k

--- src/top_k.cpp
+++ src/top_k.cpp
@@ -2,35 +2,34 @@
 #include <queue>
 #include <vector>
 
-
-// top_k finds the firm sizes associated with the k individuals with largest incomes.
+// top_k finds the elements of data_vec associated with the 
+// k largest elements in sort_vec.
+// 
 // inputs:
-//     pay = a vector of individual incomes
-//     emp = a vector of firm sizes corresponding to each individual
+//     sort_vec = a vector of individual incomes
+//     data_vec = a vector of firm sizes corresponding to each individual
 //     k = the desired number of too incomes
 
 
-
 // [[Rcpp::depends(RcppArmadillo)]]
-// [[Rcpp::plugins(cpp11)]]
 // [[Rcpp::export]]
 
 
-arma::rowvec top_k (   const arma::vec &pay,
-                        const arma::vec &emp,
-                        int k
+arma::rowvec top_k (const arma::vec &sort_vec,
+                    const arma::vec &data_vec,
+                    int k
                     )
 {
 
     std::priority_queue< std::pair<double, int>, std::vector< std::pair<double, int> >, std::greater <std::pair<double, int> > > q;
 
 
-    for (int i = 0; i < pay.size(); ++i) {
+    for (int i = 0; i < sort_vec.size(); ++i) {
         if(q.size() < k)
-            q.push(std::pair<double, int>(pay[i], i));
-        else if( q.top().first < pay[i] ){
+            q.push(std::pair<double, int>(sort_vec[i], i));
+        else if( q.top().first < sort_vec[i] ){
             q.pop();
-            q.push(std::pair<double, int>(pay[i], i));
+            q.push(std::pair<double, int>(sort_vec[i], i));
         }
     }
 
@@ -46,7 +45,7 @@ arma::rowvec top_k (   const arma::vec &pay,
     arma::rowvec output(k);
 
     for(int i = 0; i < k; i++){
-        output[i] = emp[res[i]];
+        output[i] = data_vec[res[i]];
     }
 
 
